548b0ded5016a7a258fb416001008a5d77837f1e
Christian Lindig <christian.lindig@citrix.com>
2018-08-17 16:11:02 +0100

    ocaml-407 Simplify Travis setup
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

---
+++ travis.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# This script attempts to install all upstream dependencies 
+# using OCaml 4.07 in a Docker container. This must be run 
+# from the root of the xs-opam repository.
+# 
+
+set -e
+
+IMG='ocaml/opam2:debian-9-ocaml-4.07'
+
+docker pull $IMG
+docker run --rm -iv $PWD:/mnt $IMG <<'EOF'
+set -e
+
+opam repo remove --all default
+opam repo add xs-opam file:///mnt
+
+cd /mnt
+opam depext -iy xs-toolstack
+EOF
+
+
+
